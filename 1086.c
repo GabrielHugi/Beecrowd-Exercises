a70892f4ff8fe2f993e426aa507752b19249c16e
Gabriel Hugi <gabrielhugi2@gmail.com>
2025-04-27 16:33:55 -0300

    some

--- 1086.c
+++ 1086.c
@@ -1,5 +1,6 @@
-/* beecrowd ad-hoc exercise 1086
-   made by Gabriel Loureiro Amorim Hugi
+/* 
+beecrowd ad-hoc exercise 1086
+made by Gabriel Loureiro Amorim Hugi
 */
 
 #include <stdio.h>
@@ -18,7 +19,18 @@ int main () {
   }
   /* 
   my plan to tackle this shit
-  idk
+  probably first order up bigger to smaller
+  then i can get a variable to store the ammount of planks left to fill up
+  and then i can go picking the planks like
+  if (plank[0] + plank[last] = ballroom[0 or 1]) then {
+    take the two planks and add their position to some blacklist variable
+    or maybe that variable wont be needed but anyways remove 1 from the planks
+    left to fill variable and continue doing this shit.
+  }
+  then at the end, if planks left to fill is zero at the end we say it's possible
+  and give the number of planks we used which we will also store
+  NOTE THAT WE HAVE TO DO THIS FOR BOTH X FACING AND Y FACING BOARD
+  ^ and then we compare the results and send the smaller or say its impossible
   */  
   printf("hi\n");
   return 0;
