caef664b9befac1c852da7da7069c8963edcf51f
Gabriel Hugi <gabrielhugi2@gmail.com>
2025-04-27 15:53:22 -0300

    some

--- 1086.c
+++ 1086.c
@@ -8,7 +8,18 @@ int main () {
   int ballroom[2]; // sizes x, y  centimeters
   int plankLenght; // centimeters
   int numberOfPlanks; // in planks, wood, tree, idk
-  int plankSizes[numberOfPlanks]; // centimeters  
+  scanf("%d %d", &ballroom[0], &ballroom[1]); // comes in meters
+  ballroom[0] *= 100; ballroom[1] *= 100; // fixed
+  scanf("%d", &plankLenght);
+  scanf("%d", &numberOfPlanks);
+  int plankSizes[numberOfPlanks]; // centimeters
+  for (int i = 0; i < numberOfPlanks; i++) {
+    scanf("%d", &plankSizes[i]);
+  }
+  /* 
+  my plan to tackle this shit
+  idk
+  */  
   printf("hi\n");
   return 0;
 }
