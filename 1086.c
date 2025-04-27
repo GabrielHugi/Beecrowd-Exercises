9aeaad0900dd4f7e68e0fe0d336d695cab124df5
Gabriel Hugi <gabrielhugi2@gmail.com>
2025-04-27 15:11:51 -0300

    starting this one out

---
+++ 1086.c
@@ -0,0 +1,14 @@
+/* beecrowd ad-hoc exercise 1086
+   made by Gabriel Loureiro Amorim Hugi
+*/
+
+#include <stdio.h>
+
+int main () {
+  int ballroom[2]; // sizes x, y  centimeters
+  int plankLenght; // centimeters
+  int numberOfPlanks; // in planks, wood, tree, idk
+  int plankSizes[numberOfPlanks]; // centimeters  
+  printf("hi\n");
+  return 0;
+}
