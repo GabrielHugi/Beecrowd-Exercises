4a4cd67acb35b0528643c68fdddbf7629f1c2d4e
Gabriel Loureiro Amorim Hugi <gabrielhugi2@gmail.com>
2025-04-28 17:23:39 -0300

    not working, just to save

--- 1086.c
+++ 1086.c
@@ -5,7 +5,37 @@ made by Gabriel Loureiro Amorim Hugi
 
 #include <stdio.h>
 
+// functions
+int partitionArray(int* array, int pivot, int min, int max) {
+  // its return should be posMin-1 as for the end position of the smaller than array
+  int posMin = min, posMax = max;
+  int resArr[max-min];
+  for (int i = min; i < max; i++) {
+    if (array[i] <= pivot) {
+      resArr[posMin] = array[i];
+      posMin++;
+    }
+    else {
+      resArr[posMax] = array[i];
+      posMax--;
+    }
+  }
+  return posMin-1;
+}
+
+void quickSort(int* array, int len, int min, int max) {
+  if (len == 1) return;
+  int pivot = (int)(len/2); // middle-ish
+  int info = partitionArray(array, pivot, min, max); // obscure name for fun, but explained in partitionArray()
+  // with just the position of where posMin ends we should about be able to determine the rest
+  quickSort(array, len-info+1, 0, info); // smaller
+  quickSort(array, len-info, info+1, max); // bigger
+}
+
 int main () {
+
+  /*
+  // setup
   int ballroom[2]; // sizes x, y  centimeters
   int plankLenght; // centimeters
   int numberOfPlanks; // in planks, wood, tree, idk
@@ -17,6 +47,9 @@ int main () {
   for (int i = 0; i < numberOfPlanks; i++) {
     scanf("%d", &plankSizes[i]);
   }
+  */
+
+
   /* 
   my plan to tackle this shit
   probably first order up bigger to smaller
@@ -31,7 +64,13 @@ int main () {
   and give the number of planks we used which we will also store
   NOTE THAT WE HAVE TO DO THIS FOR BOTH X FACING AND Y FACING BOARD
   ^ and then we compare the results and send the smaller or say its impossible
-  */  
-  printf("hi\n");
+  */
+  // order up 
+  int test[11] = {5, 6, 2, 0, 62, 5, 402, 64, 1, 5, 7}; 
+  quickSort(test, 11, 0, 10);
+  for (int i = 0; i < 11; i++) {
+    printf("%d, ", test[i]);
+  }
+  printf("\n");
   return 0;
 }
